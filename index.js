7820fc8c7b3af51553fa10cf65c928d90fd71d41
pavneetSingh <pavneets436@gmail.com>
2026-01-11 21:45:07 +0530

    added get, post, put, methods to handle user requests

--- index.js
+++ index.js
@@ -1,33 +1,121 @@
 
 import express from "express";
-import { PORT , mongoDBURL } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 
 const app = express();
 
-app.get('/',(request,response) => {
+// MiddleWare for parsing request body 
 
-      app.listen(PORT, () => {
-        console.log(`App is listening on Port ${PORT}`)
+app.use(express.json());
+
+
+app.get('/', (request, response) => {
+        app.listen(PORT, () => {
+                console.log(`App is listening on Port ${PORT}`)
+        });
+});
+
+// Route for Save a new Book 
+
+app.post('/books', async (request, response) => {
+
+        try {
+                if (!request.body.title || !request.body.author || !request.body.publishYear) {
+                        return response.status(400).send({
+                                message: `Send all the required fileds : title, author, publishYear`
+                        })
+                }
+                const newBook = {
+                        title: request.body.title,
+                        author: request.body.author,
+                        publishYear: request.body.publishYear,
+                }
+                const book = await Book.create(newBook);
+                // this line will send the response back to the client 
+                return response.status(201).send(book)
+
+        } catch (error) {
+                console.log(error.message)
+                response.status(500).send({ message: error.message })
+        }
+
 });
 
 
-});
+// Route to get all the books from data base 
 
+app.get('/books', async (request, response) => {
+        try {
+                const books = await Book.find({})
 
+                return response.status(200).json(
+                        {
+                                count: books.length,
+                                data: books
+                        }
+                )
+        } catch (error) {
+                console.log(error.message);
+                response.status(500).send({ message: error.message })
+        }
+}
+)
 
+// Route to get one book from the database
+
+app.get('/books/:id', async (request, response) => {
+        try {
+
+                const { id } = request.params
+
+                const book = await Book.findById(id)
+
+                return response.status(200).json(book) 
+        } catch (error) {
+                console.log(error.message);
+                response.status(500).send({ message: error.message })
+        }
+}
+)
+
+// Route to Update a Book
+
+ app.put('/books/:id', async (request,response) => {
+
+        try {  if (!request.body.title || !request.body.author || !request.body.publishYear) {
+                        return response.status(400).send({
+                                message: `Send all the required fileds : title, author, publishYear`
+                        })
+                }
+                const { id } = request.params;
+
+                const result = await Book.findByIdAndUpdate( id, request.body );
+                if (!result){
+                        return response.status(404).json({message: `Book Not Found`});
+
+                }
+                return response.status(200).json({message : `Book Updated Successfully`})
+
+        } catch (error) {
+                console.log(error.message)
+                response.status(500).send({message : error.message })
+        }
+   
+ });
 
 mongoose
-.connect(mongoDBURL)
+        .connect(mongoDBURL)
 
-.then(() => {
+        .then(() => {
 
-        app.listen(PORT, () => {
-        console.log(`App is listening on Port ${PORT}`)
+                app.listen(PORT, () => {
+                        console.log(`App is listening on Port ${PORT}`)
 
-});
-  
-}) .catch((error) => {
-        console.log(error);
-});
+                });
+
+        }).catch((error) => {
+                console.log(error);
+        });
